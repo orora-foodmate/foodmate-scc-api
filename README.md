aebe8f98d14b76bf56baf4fbc1395a4c93b928ff
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-08-28 21:57:03 +0800

    刪除不必要資訊

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Privacy operation system
+# Foodmate websocket api
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,43 +3,6 @@ import scp from 'gulp-scp2';
 import gulpSSH from 'gulp-ssh';
 import dotenv from 'dotenv';
 
-const config = dotenv.config().parsed;
-// const {BRANCH} = process.env;
-const BRANCH = 'master';
 
-function copyEnvFileTask() {
-  const sshConfig = {
-    host: '10.100.10.217',
-    username: 'useradm',
-    password: 'password'
-  };
-  const remoteServer = new gulpSSH({
-    ignoreErrors: false,
-    sshConfig
-  });
-  return remoteServer
-    .shell(`cp /var/local/envs/privacy-operation-system-api/${BRANCH}.env /var/www/html/devapi.privacy-system.net/.env`, {filePath: `${BRANCH}.env`})
-    .on('error', error => {
-      throw error;
-    });
-}
-
-function deployToServerTask() {
-  const scpConfig = {
-    host: config['SERVER_HOST'],
-    dest: config['SERVER_DEST'],
-    username: config['SERVER_USERNAME'],
-    password: config['SERVER_PASSWORD']
-  };
-  
-  return gulp
-    .src('./**/*')
-    .pipe(
-      scp(scpConfig)
-    ).on('error', error => {
-      throw error;
-    });
-}
-
-export const copyEnvFile = gulp.series(copyEnvFileTask);
-export const deploy = gulp.series(deployToServerTask);
+export const copyEnvFile = gulp.series(console.log);
+export const deploy = gulp.series(console.log);
