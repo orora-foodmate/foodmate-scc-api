2a1e533922eda37d097c18d684a1fb6ff59d9fa2
Tomas <horsekit1982@gmail.com>
2019-09-01 20:03:03 +0800

    renew moleculer framework

--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import scp from 'gulp-scp2';
-import gulpSSH from 'gulp-ssh';
-import dotenv from 'dotenv';
-
-
-export const copyEnvFile = gulp.series(console.log);
-export const deploy = gulp.series(console.log);
