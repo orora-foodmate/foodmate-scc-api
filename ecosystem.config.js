9d7e11533cf35db3c7f3b3e24f5b067faa85e2e2
Tomas <horsekit1982@gmail.com>
2019-09-17 00:48:48 +0800

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'foodmate-ws-api',
+    script: './app',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
