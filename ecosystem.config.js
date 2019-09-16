c873bbf2b09c52e41767790dadedfc9110e02818
Tomas <horsekit1982@gmail.com>
2019-09-17 00:57:00 +0800

    update script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'foodmate-ws-api',
-    script: './app',
+    script: './app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
