8b42e21a931c815ec30940754937e6fc0c77e1d8
Tomas Lin <tomas.lin@velocitylight.net>
2020-09-28 18:37:49 +0800

    add mongo

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 const {
   MONGO_URI,
   MONGO_PORT,
@@ -7,5 +8,33 @@ const {
 } = process.env;
 
 const uri = `${MONGO_URI}:${MONGO_PORT}/${MONGO_DATABASE}?poolSize=${MONGO_POOL_SIZE}`;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology:true});
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  hashPassword: {
+    type: String,
+    required: true,
+  },
+  account: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports.userModel = mongoose.model('users', userSchema);
