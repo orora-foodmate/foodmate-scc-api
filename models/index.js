69d1ce028e8633c1770fac6fabc94c66c914005b
Tomas Lin <tomas.lin@velocitylight.net>
2020-09-29 10:54:26 +0800

    拆 userSchema

--- models/index.js
+++ models/index.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-const isEmpty = require('lodash/isEmpty');
-const { saltHashPassword } = require('../helpers/utils');
 
-const { Schema } = mongoose;
 const {
   MONGO_URI,
   MONGO_PORT,
@@ -13,51 +10,6 @@ const {
 const uri = `${MONGO_URI}:${MONGO_PORT}/${MONGO_DATABASE}?poolSize=${MONGO_POOL_SIZE}`;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    default: "http://clipart-library.com/images/6cpoy78ri.png",
-  },
-  creator: {
-    type: Schema.Types.ObjectId,
-    ref: 'users' ,
-    default: null
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  hashPassword: {
-    type: String,
-  },
-  account: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  createAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updateAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-userSchema.pre('save',  function(next) {
-  console.log('AL: next', next)
-  let user = this;
-
-  // only hash the password if it has been modified (or is new)
-  if(isEmpty(user.password)) throw new Error('password is required');
-
-  user.hashPassword  = saltHashPassword(user.password);
-  next();
-});
+const userSchema = require('./userSchema');
 
 module.exports.userModel = mongoose.model('users', userSchema);
---
+++ models/userSchema.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const isEmpty = require('lodash/isEmpty');
+const { saltHashPassword } = require('../helpers/utils');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: "http://clipart-library.com/images/6cpoy78ri.png",
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'users' ,
+    default: null
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  hashPassword: {
+    type: String,
+  },
+  account: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre('save',  function(next) {
+  console.log('AL: next', next)
+  let user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if(isEmpty(user.password)) throw new Error('password is required');
+
+  user.hashPassword  = saltHashPassword(user.password);
+  next();
+});
+
+module.exports = userSchema;
