73e76377b361a212489feb3f25ff92e2c75115c2
Tomas Lin <tomas.lin@velocitylight.net>
2020-09-29 14:08:54 +0800

    調整朋友關係的 reject 和 邀請

--- routes/friendRoute.js
+++ routes/friendRoute.js
@@ -3,33 +3,65 @@ const { friendModel } = require('../models');
 const isEmpty = require('lodash/isEmpty');
 const router = express.Router();
 
+router.post('/reject/:friendId', async (req, res) => {
+  const { friendId } = req.params;
+  const creator = req.user._id;
+  const users = [friendId, creator];
+
+  try {
+    const oldRecord = await friendModel.findOne({ users, creator });
+    if (isEmpty(oldRecord) || oldRecord.status !== 1) {
+      return res.status(500).json({ success: false, data: { message: '狀態錯誤' } });
+    }
+
+    oldRecord.status = 0;
+    const result = await oldRecord.save();
+    return res.status(200).json({
+      success: true,
+      data: result
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      data: { message: error.message}
+    });
+  }
+});
+
 router.post('/invite/:friendId', async (req, res) => {
   const { friendId } = req.params;
-  const users = [friendId, req.user._id];
   const creator = req.user._id;
+  const users = [friendId, creator];
 
-  const oldRecord = friendModel.findOne({ users, creator });
+  const oldRecord = await friendModel.findOne({ users, creator });
 
-  if(!isEmpty(oldRecord) && oldRecord.status !== 0) {
-    return res.status(500).json({ success: false, data: { message: '狀態錯誤' } });
-  }
-
-  const friend = new friendModel({
-    users,
-    creator: req.user._id,
-    status: 1
-  });
-
-  friend.save(error => {
-    if (!isEmpty(error)) {
-      return res.status(500).json({ success: false, data: { message: error.message } });
-    }
+  if (isEmpty(oldRecord)) {
+    const friend = await friendModel.insert({
+      users,
+      creator,
+      status: 1
+    });
 
     return res.status(200).json({
       success: true,
       data: friend
-    })
-  })
+    });
+  }
+
+  if (oldRecord.status === 0) {
+    oldRecord.status = 1;
+    const result = await oldRecord.save();
+
+    return res.status(200).json({
+      success: true,
+      data: result
+    });
+  }
+
+  return res.status(500).json({
+    success: false,
+    data: { message: '狀態錯誤' }
+  });
 });
 
 module.exports = router;
