bd85e7dda7635f87726bf9c7f4e0ef0660d81eaa
Tomas Lin <horsekit1982@gmail.com>
2020-10-01 15:24:07 +0800

    包含訊息的時間搜尋

--- socketEvents/messagesEvents.js
+++ socketEvents/messagesEvents.js
@@ -6,12 +6,17 @@ const getMessagesLisenter = async (socket) => {
   // Set up a loop to handle and respond to RPCs.
   for await (let request of socket.procedure('getRooms')) {
     const {roomId, startDateTime} = request.data;
+    console.log("forawait -> startDateTime", startDateTime)
     const date = startOfDay(new Date(startDateTime));
 
     try {
+      console.log("forawait -> new Date(startDateTime)", new Date(startDateTime))
       const messages = await messageModel.find({
         room: mongoose.Types.ObjectId(roomId),
         date: {"$gte": date},
+        "messages.createAt": {
+          "$gte": new Date(startDateTime)
+        }
       });
       request.end(messages)
     } catch (error) {
