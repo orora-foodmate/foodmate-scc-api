f4451bd74df0e2f5c1fbfbf30050ccc7325b52b2
Tomas Lin <horsekit1982@gmail.com>
2020-11-02 22:44:31 +0800

    add phone gender fields

--- models/userSchema.js
+++ models/userSchema.js
@@ -32,10 +32,21 @@ const userSchema = new Schema({
     unique: true,
     required: true,
   },
+  phone: {
+    type: String,
+    unique: true,
+    default: null,
+  },
   regId: {
     type: String,
     unique: true,
     default: '',
+  },
+  gender: {
+    type: Number,
+    unique: true,
+    default: null,
+    enum: [null, 'M', 'F']
   },
   createAt: {
     type: Date,
