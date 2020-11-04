c01328c24395a9e263d263fde93301a1f568188d
Tomas Lin <horsekit1982@gmail.com>
2020-11-04 22:37:46 +0800

    update json

--- firebaseNotic/app.js
+++ firebaseNotic/app.js
@@ -1,22 +1,25 @@
 const admin = require("firebase-admin");
 const foodmateConfig = require("./serviceAccountKey/foodmateConfig");
+const serviceAccount = require('./serviceAccountKey/foodmate-d602a-firebase-adminsdk-py14o-f73858b938.json');
+console.log("serviceAccount", serviceAccount)
 
-console.log('admin.credential.applicationDefault()', admin.credential.applicationDefault())
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  ...foodmateConfig,
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://foodmate-d602a.firebaseio.com"
 });
 
-admin.messaging().send({
-  token: "dZNDj2oP8EyvgHqPvGd_L4:APA91bGkWlVXXsyQ3lc8HU26Te91NgR_UMjsA7sVn7aiZ3adzH-5M2IvNqmoYA442ikPOxUtDNO0ktsB7Cd_r7Sj1TzOoYThtjl-GM8_OCRbymez3cNFOUa5kxsPtEAxUdPFtgZYQpYA",
-  notification: {
-    title: 'test title',
-    body: 'test body'
-  }
-}).then(resp => {
-console.log('resp', resp)
+console.log(admin.name);
+
+// admin.messaging().send({
+//   token: "d7wM62CR0UNzqY69mWaNDM:APA91bEHA5UIvgq-CvZx8o2K4BoC1c2ZJlHno6l6nEt1fJsTTCl-0_hvlYHEpcWma57zJDL01F-1UWmKZrhXBEo-GE8kwPz9YhaXYjiZORsntSlqj9NbhjqJzGslJ0Msvua2kWEFuvYO",
+//   notification: {
+//     title: 'test title',
+//     body: 'test body'
+//   }
+// }).then(resp => {
+// console.log('resp', resp)
   
-}).catch(error => {
-console.log('error', error)
+// }).catch(error => {
+// console.log('error', error)
   
-})
+// })
