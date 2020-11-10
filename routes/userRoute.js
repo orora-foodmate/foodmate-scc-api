6403b4259e166f97a9bed59d6ec85c7b0e42ac6d
Tomas Lin <horsekit1982@gmail.com>
2020-11-11 02:19:12 +0800

    update register _id => id

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const yup = require('yup');
 const { userModel } = require('../models');
 const pick = require("lodash/pick");
 const router = express.Router();
@@ -26,8 +27,16 @@ router.get('/:id', tokenVerifyMiddleware, async (req, res) => {
 
 });
 
+const createNewUserSchema = yup.object().shape({
+  account: yup.string().required("account 不可為空"),
+  name: yup.string().required("name 不可為空"),
+  password: yup.string().required("password 不可為空"),
+});
+
 router.post('/', async (req, res) => {
   try {
+    await createNewUserSchema.validate(req.body);
+
     const { name, password, account } = req.body;
     const id = mongoose.Types.ObjectId();
     const user = new userModel({
@@ -41,7 +50,7 @@ router.post('/', async (req, res) => {
     // 之後要拆出 micro service 控制 online status 時使用
     // await req.exchange.invokePublish('createNewUserStatus', { userId: id });
 
-    const myTokenData = pick(user, ["_id", "account", "avatar", "name"]);
+    const myTokenData = pick(user, ["id", "account", "avatar", "name"]);
     const signedTokenString = await agServer.auth.signToken(
       myTokenData,
       agServer.signatureKey
